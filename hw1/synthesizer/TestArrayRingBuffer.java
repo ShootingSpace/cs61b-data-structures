25ac246f53b905c2cc24561fc50ab583e0d53285
Cong <erdongerzong@qq.com>
2018-06-17 14:43:59 +0100

    Task 3: ArrayRingBuffer

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,27 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        for (int i=0; i<arb.capacity(); i++){
+            arb.enqueue(i);
+        }
+        assertEquals(0, arb.dequeue());
+        assertEquals(1, arb.peek());
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void underFlowTest() {
+        ArrayRingBuffer arb = new ArrayRingBuffer(1);
+        arb.dequeue();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void overFlowTest() {
+        ArrayRingBuffer arb = new ArrayRingBuffer(1);
+        for (int i=0; i<=arb.capacity(); i++){
+            arb.enqueue(i);
+        }
     }
 
     /** Calls tests for ArrayRingBuffer. */
