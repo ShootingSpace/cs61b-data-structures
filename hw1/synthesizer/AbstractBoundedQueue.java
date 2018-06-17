e20910e01d824a0c71f52aa93c0dfde7bb298baf
Cong <erdongerzong@qq.com>
2018-06-17 14:43:27 +0100

    Task 2: AbstractBoundedQueue

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,17 @@
+package synthesizer;
+
+/** To provide a protected fillCount and capacity variable
+ * so that all subclasses will inherit, as well as getter methods capacity() and fillCount().
+ * This saves a tiny amount of work for future implementations.
+ */
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
+    protected int fillCount;
+    protected int capacity;
+    public int capacity(){
+        return capacity;
+    }
+    public int fillCount(){
+        return fillCount;
+    }
+
+}
